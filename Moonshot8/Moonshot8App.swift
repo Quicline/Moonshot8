31505bcc1ca11e5b1e29bd089f9ef97eaaba36a2
Armando Francisco <armando17171771@gmail.com>
2024-11-06 10:33:26 -0500

    Initial Commit

---
+++ Moonshot8/Moonshot8App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot8App.swift
+//  Moonshot8
+//
+//  Created by Armando Francisco on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Moonshot8Tests/Moonshot8Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot8Tests.swift
+//  Moonshot8Tests
+//
+//  Created by Armando Francisco on 11/6/24.
+//
+
+import Testing
+@testable import Moonshot8
+
+struct Moonshot8Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
