340e2232d73c4c830fd2b059914fe248e55b0068
Armando Francisco <armando17171771@gmail.com>
2024-11-07 06:40:04 -0500

    Showing mission details with ScrollView and containerRelativeFrame()

---
+++ Moonshot8/MissionView.swift
@@ -0,0 +1,47 @@
+//
+//  MissionView.swift
+//  Moonshot8
+//
+//  Created by Armando Francisco on 11/7/24.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                        .font(.body)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+    //MissionView()
+}
